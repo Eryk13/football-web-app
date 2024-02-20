2fdde4fb8ec7d6310cabeb6179c345461899411f
Eryk <eryk9903@gmail.com>
2024-02-20 15:06:52 +0100

    create nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <script setup>
-
 </script>
 
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
+    <RouterLink id="logo" to="/">Football</RouterLink>
+    <nav>
+      <RouterLink class="link" :to="{name: 'home'}">Home</RouterLink>
+      <RouterLink class="link" :to="{name: 'admin'}">Admin</RouterLink>
+    </nav>
   </header>
-
   <RouterView />
 </template>
 
 <style scoped>
+  header{
+    border-bottom: solid 1px black;
+    padding: 1em 10em;
+    display: flex;
+    background-color: var(--color-primary);
+  }
+
+  #logo{
+    font-size: 2rem;
+    font-weight: 900;
+    font-family: "Anta", sans-serif;
+    margin-right: 2rem;
+  }
+
+  nav{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .link{
+    font-family: "Anta", sans-serif;
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+
+  .link:hover{
+    color: white;
+  }
 </style>
